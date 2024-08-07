b7623e43f154e57cd47526d550e985b824caad22
tnie <tnie@tuta.io>
2024-08-07 00:02:07 -0500

    do not install config files on darwin

--- install.sh
+++ install.sh
@@ -16,17 +16,6 @@ else
     echo Oh My Zsh already installed, skipping.
 fi
 
-# Install dotfiles
-echo "Copying dotfiles to $HOME_DIR"
-cp -r $CWD/dotfiles/. $HOME_DIR
-
-# Install config files
-if [ ! -d $CONFIG_INSTALL_DIR ]; then
-    mkdir $CONFIG_INSTALL_DIR
-fi
-echo "Copying config files to $CONFIG_INSTALL_DIR"
-cp -r $CWD/.config/* $CONFIG_INSTALL_DIR
-
 # Install oh my tmux
 if [[ -d "$OHMYTMUX_DIR" ]]; then
     echo Oh My Tmux already installed, skipping.
@@ -36,4 +25,22 @@ else
     git clone https://github.com/gpakosz/.tmux.git > /dev/null
     ln -s -f .tmux/.tmux.conf
     cd $CWD
+fi
+
+# Install dotfiles
+echo "Copying dotfiles to $HOME_DIR"
+cp -r $CWD/dotfiles/. $HOME_DIR
+
+
+# Install config files only if system is Linux
+if [[ $(uname) = "Linux" ]]; then
+
+    if [ ! -d $CONFIG_INSTALL_DIR ]; then
+        mkdir $CONFIG_INSTALL_DIR
+    fi
+    echo "Copying config files to $CONFIG_INSTALL_DIR"
+    cp -r $CWD/.config/* $CONFIG_INSTALL_DIR
+
+else
+    echo "Skipping installation of config files as the system is not Linux."
 fi
