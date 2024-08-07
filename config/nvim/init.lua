2d3fa094b81e883b74de2d8721298160dae9fe67
tnie <tnie@tuta.io>
2024-08-07 00:10:45 -0500

    nvim remove smooth scroll and fugitive

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -54,7 +54,6 @@ require('packer').startup(function()
 
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'tpope/vim-fugitive' }
 
   -- File explorer
   use {
@@ -181,14 +180,6 @@ require('packer').startup(function()
     end
   }
 
-  -- smooth scroll
-  use {
-    'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup()
-    end
-  }
-
   -- Slime
   use {
     'jpalardy/vim-slime',
@@ -263,77 +254,7 @@ augroup end
 
 
 -- Gitsigns
-require('gitsigns').setup {
-  signs                        = {
-    add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-  },
-  signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-  numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  watch_gitdir                 = {
-    interval = 1000,
-    follow_files = true
-  },
-  attach_to_untracked          = true,
-  current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts      = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-    ignore_whitespace = false,
-  },
-  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-  sign_priority                = 6,
-  update_debounce              = 100,
-  status_formatter             = nil, -- Use default
-  max_file_length              = 40000,
-  preview_config               = {
-    -- Options passed to nvim_open_win
-    border = 'single',
-    style = 'minimal',
-    relative = 'cursor',
-    row = 0,
-    col = 1
-  },
-  yadm                         = {
-    enable = false
-  },
-  on_attach                    = function(bufnr)
-    local function map(mode, lhs, rhs, opts)
-      opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
-      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-    end
-
-    -- Navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
-
-    -- Actions
-    map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-    map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-    map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
-
-    -- Text object
-    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
-}
-
+require('gitsigns').setup{}
 
 -- LSP settings
 local opts = { noremap = true, silent = true }
@@ -365,10 +286,7 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- require("mason").setup()
-require("mason").setup {
-  log_level = vim.log.levels.DEBUG
-}
+require("mason").setup()
 local lspconfig = require("lspconfig")
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
