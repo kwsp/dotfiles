f57473f1c345f48651110442ef79dc8b1cf43475
tnie <tnie@tuta.io>
2024-12-01 01:24:17 -0600

    updated nvim cpp template

--- config/nvim/templates/skeleton.cpp
+++ config/nvim/templates/skeleton.cpp
@@ -1,9 +1,19 @@
+#include <format>
 #include <iostream>
-#include <vector>
+#include <numeric>
+#include <sstream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
+
+  std::string line;
+  while (getline(cin, line)) {
+    istringstream ss(line);
+    // TODO
+  }
 }
