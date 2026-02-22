31cf78108bc566971d21d44c8ca0233378bd2e4d
tnie <tnie@tuta.io>
2026-02-22 14:30:40 +0800

    fix bugs

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ UNAME=$(uname)
 
 # cd into script dir
 BASEDIR=$(dirname "$0")
-cd $BASEDIR
+cd "$BASEDIR"
 
 # redirect stdout/stderr to a file
 #exec &>> log.txt
@@ -15,39 +15,17 @@ adddate() {
     done
 }
 
-gitupdate() {
-
-    if [[ $# = 0 ]]; then
-        echo "no args"
-        commit_msg="Updated config $(date)"
-    else
-        commit_msg=$1
-    fi
-
-    changed=$(git diff --name-only HEAD)
-    if [[ $changed ]]; then
-        echo changed files: $changed
-        echo uploading to github
-        git add . > /dev/null
-        git commit -m "$commit_msg" > /dev/null
-        git push > /dev/null
-    else
-        echo nothing changed
-    fi
-}
-
 main() {
     echo "Starting backup ..."
 
     # backup config
-    rsync -q -av ~/.config/sway ~/.config/waybar ~/.config/kitty ~/.config/nvim ~/.config/zathura ~/.config/fastfetch config
+    rsync -av ~/.config/sway ~/.config/waybar ~/.config/kitty ~/.config/nvim ~/.config/zathura ~/.config/fastfetch config
     # backup dotfiles
-    rsync -q -av ~/.tmux.conf.local ~/.zshrc ~/.gitconfig dotfiles/
+    rsync -av ~/.tmux.conf.local ~/.zshrc ~/.gitconfig dotfiles/
 
-    #gitupdate $1
     echo "Backup complete."
 }
 
 
-main $@ | adddate
+main "$@" | adddate
 echo
--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
+CWD=$(cd "$(dirname "$0")" && pwd)
 HOME_DIR=~
-CONFIG_INSTALL_DIR=$HOME_DIR/.config/
-OHMYZSH_DIR=$HOME_DIR/.oh-my-zsh
-OHMYTMUX_DIR=$HOME_DIR/.tmux
-CWD=$(pwd)
+CONFIG_INSTALL_DIR="$HOME_DIR/.config"
+OHMYZSH_DIR="$HOME_DIR/.oh-my-zsh"
+OHMYTMUX_DIR="$HOME_DIR/.tmux"
 
-# echo "$(tput setaf 2)WORKDIR: $CWD"
-echo $(tput setaf 4)Hello!
+echo "$(tput setaf 4)Hello!"
 
 # Install oh my zsh
-[[ -z "${ZSH}" ]] && echo "$(tput setaf 3)Installing Oh My Zsh . . ." && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || echo "$(tput setaf 2)Oh My Zsh already installed."
+if [[ ! -d "$OHMYZSH_DIR" ]]; then
+    echo "$(tput setaf 3)Installing Oh My Zsh . . ."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "$(tput setaf 2)Oh My Zsh already installed."
+fi
 
 # Install oh my tmux
 if [[ -d "$OHMYTMUX_DIR" ]]; then
@@ -20,17 +25,17 @@ else
     cd
     git clone https://github.com/gpakosz/.tmux.git > /dev/null
     ln -s -f .tmux/.tmux.conf
-    cd $CWD
+    cd "$CWD"
 fi
 
 # Install dotfiles
 echo "$(tput setaf 3)Copying dotfiles to $HOME_DIR . . ."
-cp -r $CWD/dotfiles/. $HOME_DIR
+cp -r "$CWD/dotfiles/." "$HOME_DIR"
 
 # Install config
-[ -d $CONFIG_INSTALL_DIR ] || mkdir $CONFIG_INSTALL_DIR
+[ -d "$CONFIG_INSTALL_DIR" ] || mkdir "$CONFIG_INSTALL_DIR"
 echo "$(tput setaf 3)Copying config files to $CONFIG_INSTALL_DIR . . ."
-cp -r $CWD/config/* $CONFIG_INSTALL_DIR
+cp -r "$CWD/config/." "$CONFIG_INSTALL_DIR"
 
 ### Install platform specific config
 case "$(uname -s)" in
@@ -38,7 +43,6 @@ case "$(uname -s)" in
     ;;
 
   Darwin*)
-    echo
     ;;
 esac
 
